ccc2399ef845fcfd3b8f216199d0330233fb1834
Poós Dániel <poosdani@gmail.com>
2021-09-28 12:13:42 +0200

    php

--- gitlearn.php
+++ gitlearn.php
@@ -11,5 +11,6 @@
     <p>Nice , yo nice</p>
     <p>username set:<br> git config --local user.name "Name Name"</p>
     <p>email set:<br> git config --local user.email "email@address.com"</p>
+    <?php echo "<h4>Do you still...</h4>"; ?>
 </body>
 </html>
